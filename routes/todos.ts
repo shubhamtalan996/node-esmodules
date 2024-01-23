3192196543988a379fc07bfc9974eb24d59339d0
Shubham Talan <shubhamsinghtalan08@gmail.com>
2024-01-23 16:53:46 +0530

    feat(todo): added CRUD apis for basic todo app

---
+++ routes/todos.ts
@@ -0,0 +1,54 @@
+import { Router } from "express";
+import { ITodo } from "../models/todo";
+
+const router = Router();
+
+let todos: ITodo[] = [];
+
+router.get("/", (req, res, next) => {
+  res.status(200).json({
+    todos,
+  });
+});
+
+router.post("/todo", (req, res, next) => {
+  const newTodo = {
+    id: new Date().toISOString(),
+    text: req.body.text,
+  };
+  todos.push(newTodo);
+  res.status(201).json({
+    message: "todo added",
+    todo: newTodo,
+    todos,
+  });
+});
+
+router.put("/todo/:todoId", (req, res, next) => {
+  const tid = req.params.todoId;
+  const todoIndex = todos.findIndex(({ id }) => id === tid);
+  if (todoIndex < 0) {
+    return res.status(404).json({
+      message: "todo not found",
+      todos,
+    });
+  }
+  todos[todoIndex].text = req.body.text;
+
+  res.status(200).json({
+    message: "todo updated",
+    todos,
+  });
+});
+
+router.delete("/todo/:todoId", (req, res, next) => {
+  const tid = req.params.todoId;
+  todos = todos.filter(({ id }) => id !== tid);
+
+  res.status(200).json({
+    message: "Deleted todo!",
+    todos,
+  });
+});
+
+export default router;
